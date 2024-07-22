fc8591915f76d34559c03d1d2908edc3a000aed6
Eugene Milford <gmilford321@gmail.com>
2024-07-22 21:17:39 +0200

    CRUD Functionality Added to Accounts

--- obj/Debug/net8.0/BankingAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/BankingAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72e908029e43bb5edd922fafa2ee8f38d77101e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99e83bfdc06a92962b92e9a657977832e0408f46")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
