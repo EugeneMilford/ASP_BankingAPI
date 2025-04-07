fbde38e738b1bb6a6f0e06bf9a45391d6f65385a
Eugene Milford <gmilford321@gmail.com>
2025-04-07 23:15:17 +0200

    Models Updated

--- obj/Debug/net8.0/BankingAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/BankingAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e29642a6b4193d73348dce748f89d8b9544ee56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7b0ea9eb0881cacdd7ec465ffc703e5937d3c9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
